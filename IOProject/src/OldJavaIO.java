064ebd59c110ba845e456829c21c7418b0b957cc
evcat <evcat@Turtle>
2016-03-17 15:15:42 +0300

    Added file "text.txt" and wrote character Input from file without TWR

--- IOProject/src/OldJavaIO.java
+++ IOProject/src/OldJavaIO.java
@@ -1,8 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class OldJavaIO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		StringBuffer buff = new StringBuffer();
+		
+		FileInputStream myFile = null;
+		
+		try {
+			myFile = new FileInputStream("/home/evcat/git/IO/IOProject/bin/text.txt");
+			
+			InputStreamReader inpReader = new InputStreamReader(myFile, "UTF8");
+			
+			BufferedReader reader = new BufferedReader(inpReader);
+			
+			int c = 0;
+			
+			while((c=reader.read()) > -1) {
+				buff.append((char)c);
+			}
+			System.out.print(buff.toString());
+		} catch (IOException e) {
+			System.out.print("Couldn't read file: " + e.getMessage());
+		} finally {
+			if (myFile != null) {
+				try {
+					myFile.close();
+				} catch (IOException e1) {
+					e1.getMessage();
+				}
+			}
+		}
 		
 	}
 
